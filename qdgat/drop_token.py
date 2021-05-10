ae4ff0f6a8c9bc4d94d230674e0d30e340c26000
Niels Rogge <niels.rogge1@gmail.com>
2021-05-10 19:57:42 +0200

    New fix

--- qdgat/drop_token.py
+++ qdgat/drop_token.py
@@ -2,7 +2,8 @@ from allennlp.data.tokenizers import Token as OToken
 
 
 class Token(OToken):
-    def __new__(cls, text: str = None,
+    def __init__(self, 
+        text: str = None,
         idx: int = None,
         lemma_: str = None,
         pos_: str = None,
@@ -10,16 +11,7 @@ class Token(OToken):
         dep_: str = None,
         ent_type_: str = None,
         text_id: int = None,
-        edx: int = None
-    ):
-        self = super(Token, cls).__new__(cls)
-        self.text = text
-        self.idx = idx
-        self.lemma_ = lemma_
-        self.pos_ = pos_
-        self.tag_ = tag_
-        self.dep_ = dep_
-        self.ent_type_ = ent_type_
-        self.text_id =  text_id
+        edx: int = Nones):
+        
+        super().__init__(text, idx, lemma_, pos_, tag_, dep_, ent_type_, text_id,
         self.edx = edx
-        return self
